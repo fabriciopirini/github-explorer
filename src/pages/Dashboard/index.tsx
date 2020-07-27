04736230b950b5bcb54578d820d56302b8ec1547
Fabricio Pirini <fabricio.pirini@gmail.com>
2020-07-27 16:06:18 +0200

    Add and show repository details

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,58 +1,98 @@
-import React from 'react'
+import React, { useState, FormEvent, useEffect } from 'react'
 import { FiChevronRight } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
+
+import api from '../../services/api'
 
 import logoImg from '../../assets/logo.svg'
 
-import { Title, Form, Repositories } from './styles'
+import { Title, Form, Repositories, Error } from './styles'
+
+interface Repository {
+  full_name: string
+  description: string
+  owner: {
+    login: string
+    avatar_url: string
+  }
+}
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('')
+  const [inputError, setInputError] = useState('')
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem(
+      '@GithubExplorer:repositories',
+    )
+
+    if (storagedRepositories) return JSON.parse(storagedRepositories)
+    else return []
+  })
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories),
+    )
+  }, [repositories])
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault()
+
+    if (!newRepo) {
+      setInputError('Type the author/name of the repository')
+      return
+    }
+
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`)
+
+      const repository = response.data
+
+      setRepositories([...repositories, repository])
+      setNewRepo('')
+      setInputError('')
+    } catch (error) {
+      setInputError('Error raised when searching for repository')
+    }
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore Github repositories</Title>
 
-      <Form>
-        <input placeholder="The repository's name" />
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Search for a repository (format: author/repo_name)"
+        />
         <button type="submit">Search</button>
       </Form>
 
+      {inputError && <Error>{inputError}</Error>}
+
       <Repositories>
-        <a href="test">
-          <img
-            src="https://avatars1.githubusercontent.com/u/20199659"
-            alt="Fabricio Pirini"
-          />
-          <div>
-            <strong>rocketseat/unform</strong>
-            <p>Easy peesy highly scalable ReactJS & React Native forms!</p>
-          </div>
+        {repositories.map(repository => (
+          <Link
+            key={repository.full_name}
+            to={`/repositories/${repository.full_name}`}
+          >
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-        <a href="test">
-          <img
-            src="https://avatars1.githubusercontent.com/u/20199659"
-            alt="Fabricio Pirini"
-          />
-          <div>
-            <strong>rocketseat/unform</strong>
-            <p>Easy peesy highly scalable ReactJS & React Native forms!</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="test">
-          <img
-            src="https://avatars1.githubusercontent.com/u/20199659"
-            alt="Fabricio Pirini"
-          />
-          <div>
-            <strong>rocketseat/unform</strong>
-            <p>Easy peesy highly scalable ReactJS & React Native forms!</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </Link>
+        ))}
       </Repositories>
     </>
   )
